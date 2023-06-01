8505b5cda9c321732d7d3601adff5492766d8ec8
airglow923 <p.hyundeok76@gmail.com>
2023-06-02 00:34:46 +0900

    feat: update 'build-docker.sh' - Make 'NGINX_VERSION' argument

--- alpine/build-docker.sh
+++ alpine/build-docker.sh
@@ -6,14 +6,14 @@ ROOT_DIR=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 
 TAG="${1:-$(basename "$(dirname "$(pwd)")")}"
 
-cd "${ROOT_DIR}"
-
-NGINX_VERSION="$(
+NGINX_VERSION="${2:-$(
     curl \
         --silent \
         --fail \
         https://hg.nginx.org/nginx/raw-file/default/src/core/nginx.h |
         sed -n 's/#define NGINX_VERSION\s*"\(.*\)"/\1/p'
-)"
+)}"
+
+cd "${ROOT_DIR}"
 
 docker build -t "${TAG}" --progress=plain --build-arg NGINX_VERSION="${NGINX_VERSION}" .
